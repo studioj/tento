d69a992c644597683a88199780314e9458eb77d5
studioj <22102283+studioj@users.noreply.github.com>
2024-04-26 15:37:28 +0200

    initial commit

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""Empty initialization file for tento package"""
+
+def say_hi():
+  """raise a simple exception message"""
+  raise Exception("Hi from tento package, you've reached the wrong package please use the internal pypi package!")
+
+# Add more functions or classes here as needed
+
+say_hi()
